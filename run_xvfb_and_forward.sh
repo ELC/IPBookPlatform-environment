11beeb2a82c4559ff1946125bcb2a075b5c08ff7
Ezequiel Leonardo Castaño <14986783+ELC@users.noreply.github.com>
2025-03-04 17:28:13 +0000

    refactor(script): remove unused LIBGL_ALWAYS_SOFTWARE export
    - Eliminate the export of LIBGL_ALWAYS_SOFTWARE as it is no longer
      necessary for the script's functionality.

--- run_xvfb_and_forward.sh
+++ run_xvfb_and_forward.sh
@@ -7,7 +7,6 @@ Xvfb :1 -screen 0 1024x768x16 &
 export USER=ubuntu
 export DISPLAY=:1
 export OCTAVE_EXECUTABLE="$(command -v octave)"
-export LIBGL_ALWAYS_SOFTWARE=1
 
 # Wait a bit to ensure Xvfb is fully started
 sleep 2
